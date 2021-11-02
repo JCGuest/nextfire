0a8d133766b3cafa6c18cb8034af7a87c64d448f
JCGuest <jcguest90@gmail.com>
2021-11-01 20:04:55 -0500

    add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
-
 import { GetServerSideProps } from 'next';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return ( 
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  ) 
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Loader from '../components/Loader'
 
 export default function Home() {
     return ( 
         <div>
-            <Link prefetch={false} href={{
-                pathname: '/[username]',
-                query: { username: 'spaceCowboy'},
-            }}>
-                <a>spaceCowboy's profile</a>
-            </Link>
-
+            <Loader show/>
         </div>
     )
 }
